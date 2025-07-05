2504ac521904b33cd4d0639d1a6211bfed5745f1
burity2 <arthur.rosa66@gmail.com>
2025-07-05 10:00:02 -0300

    Fixed subsequential button pressing giving results

--- js.js
+++ js.js
@@ -12,35 +12,52 @@ const multiply = (a, b) => {
 };
 
 const divide = (a, b) => {
+    if (b == 0) {
+    return "( ͡° ͜ʖ ͡° )"
+    } else {
     return a / b;
+    }
+  
 };
 
 const operate = (a, n, b) => {
-    if (n == "e") {
+    if (n == "+") {
         return add(a, b);
-    } else if (n == "s"){
+    } else if (n == "−"){
         return subtract(a, b);
-    } else if (n == "m"){
+    } else if (n == "x"){
         return multiply(a, b);
-    } else if (n == "d"){
+    } else {
         return divide(a, b);
     }
 };
 
+//function that highlights buttons (under work)
+const highlighter = (buttonId) => {
+    const operButtons = document.getElementsByClassName("oper");
+    for (i=0; i <= operButtons.length; i++) {
+        operButtons[i].setAttribute("style", "background-color = #69809e;");
+    }
+    document.getElementById(buttonId).setAttribute("style", "background-color = #95c5ac;");
+
+}
+
 // VISOR
 const visor = document.getElementById("visor")
 // var that stores the numbers
-const visorDisplay = []
-
+let visorDisplay = []
+visor.innerText = 0
+let n = 0
+// this puts the numbers into the visor
 const buttonToVisor = document.getElementById("container")
-buttonToVisor.addEventListener("click", (event) => {
+const buttonPusher = buttonToVisor.addEventListener("click", (event) => {
     const buttonClicked = event.target;
     if (visorDisplay.length >= 14) {
         
     } else {
     switch(buttonClicked.id) {
         case "7":
-            visorDisplay.push(7);
+            visor.innerHTML = visorDisplay.push(7);
             visor.innerHTML = visorDisplay.toString().replace(/,/g,"")
             break;
         case "8":
@@ -81,7 +98,10 @@ buttonToVisor.addEventListener("click", (event) => {
             break;
         case "dot":
             if (visorDisplay.includes(".") == true) {
-
+            break;
+            } else if (visor.innerText = 0){
+            visor.innerHTML = visorDisplay.push("0.")
+            visor.innerHTML = visorDisplay.toString().replace(/,/g,"")
             } else {
                 visor.innerHTML = visorDisplay.push(".")
             visor.innerHTML = visorDisplay.toString().replace(/,/g,"")
@@ -91,13 +111,157 @@ buttonToVisor.addEventListener("click", (event) => {
 }
 })
 
+const plusButton = document.getElementById("plus");
+plusButton.addEventListener("click", () => {
+    let visorNumber = visorDisplay.toString().replace(/,/g,"")
+    if (n == "+") {
+        visor.innerText = operate(+a, n, +visorNumber)
+        a = visor.innerText
+        visorDisplay = []
+
+    } else {
+        a = visorNumber
+        document.getElementById("plus").style.backgroundColor = "#95c5ac";
+        document.getElementById("minus").style.backgroundColor = "#69809e";
+        document.getElementById("times").style.backgroundColor = "#69809e";
+        document.getElementById("divide").style.backgroundColor = "#69809e";
+        n = "+"
+        visorDisplay = []
+    }
+})
+
+const minusButton = document.getElementById("minus");
+minusButton.addEventListener("click", () => {
+    let visorNumber = visorDisplay.toString().replace(/,/g,"")
+    if (n == "−") {
+        visor.innerText = operate(+a, n, +visorNumber)
+        a = visor.innerText
+        visorDisplay = []
+
+    } else {
+        a = visorNumber
+        document.getElementById("plus").style.backgroundColor = "#69809e";
+        document.getElementById("minus").style.backgroundColor = "#95c5ac";
+        document.getElementById("times").style.backgroundColor = "#69809e";
+        document.getElementById("divide").style.backgroundColor = "#69809e";
+        n = "−"
+        visorDisplay = []
+    }
+})
+
+const timesButton = document.getElementById("times");
+timesButton.addEventListener("click", () => {
+    let visorNumber = visorDisplay.toString().replace(/,/g,"")
+    if (n == "x") {
+        visor.innerText = operate(+a, n, +visorNumber)
+        a = visor.innerText
+        visorDisplay = []
+
+    } else {
+        a = visorNumber
+        document.getElementById("plus").style.backgroundColor = "#69809e";
+        document.getElementById("minus").style.backgroundColor = "#69809e";
+        document.getElementById("times").style.backgroundColor = "#95c5ac";
+        document.getElementById("divide").style.backgroundColor = "#69809e";
+        n = "x"
+        visorDisplay = []
+    }
+})
+
+const divideButton = document.getElementById("divide");
+divideButton.addEventListener("click", () => {
+    let visorNumber = visorDisplay.toString().replace(/,/g,"")
+    if (n == "÷") {
+        visor.innerText = operate(+a, n, +visorNumber)
+        a = visor.innerText
+        visorDisplay = []
+
+    } else {
+        a = visorNumber
+        document.getElementById("plus").style.backgroundColor = "#69809e";
+        document.getElementById("minus").style.backgroundColor = "#69809e";
+        document.getElementById("times").style.backgroundColor = "#69809e";
+        document.getElementById("divide").style.backgroundColor = "#95c5ac";
+        n = "÷"
+        visorDisplay = []
+    }
+})
+
+const equalButton = document.getElementById("eq")
+equalButton.addEventListener("click", () => {
+    let visorNumber = visorDisplay.toString().replace(/,/g,"")
+
+            document.getElementById("plus").style.backgroundColor = "#69809e";
+            document.getElementById("minus").style.backgroundColor = "#69809e";
+            document.getElementById("times").style.backgroundColor = "#69809e";
+            document.getElementById("divide").style.backgroundColor = "#69809e";
+            visor.innerText = operate(+a, n, +visorNumber)
+            n = 0
+            visorDisplay = []
+
+})
+
+
+const clearButton = document.getElementById("clear")
+clearButton.addEventListener("click", () => {
+            a = 0
+            n = 0
+            visorDisplay = []
+            visor.innerText = 0
+            document.getElementById("plus").style.backgroundColor = "#69809e";
+            document.getElementById("minus").style.backgroundColor = "#69809e";
+            document.getElementById("times").style.backgroundColor = "#69809e";
+            document.getElementById("divide").style.backgroundColor = "#69809e";
+    })
+
 ```
+const operatorButtons = document.getElementById("operator-buttons")
+operatorButtons.addEventListener("click", (event) => {
+    let visorNumber = visorDisplay.toString().replace(/,/g,"")
+    a = visorNumber
+    const buttonClicked = event.target;
+
+    switch(buttonClicked.id) {
         case "plus":
+            if (n == "+"){
+                let visorNumber = visorDisplay.toString().replace(/,/g,"")
+                visor.innerText = operate(+a, n, +visorNumber)
+                visorDisplay = []
+                
+            } else {
+            document.getElementById("plus").style.backgroundColor = "#95c5ac";
+            document.getElementById("minus").style.backgroundColor = "#69809e";
+            document.getElementById("times").style.backgroundColor = "#69809e";
+            document.getElementById("divide").style.backgroundColor = "#69809e";
+            n = "+"
+            visorDisplay = []
+            }
             break;
         case "minus":
+            document.getElementById("plus").style.backgroundColor = "#69809e";
+            document.getElementById("minus").style.backgroundColor = "#95c5ac";
+            document.getElementById("times").style.backgroundColor = "#69809e";
+            document.getElementById("divide").style.backgroundColor = "#69809e";
+            n = "−"
+            visorDisplay = []
             break;
         case "times":
+            document.getElementById("plus").style.backgroundColor = "#69809e";
+            document.getElementById("minus").style.backgroundColor = "#69809e";
+            document.getElementById("times").style.backgroundColor = "#95c5ac";
+            document.getElementById("divide").style.backgroundColor = "#69809e";
+            n = "x"
+            visorDisplay = []
             break;
         case "divide":
+            document.getElementById("plus").style.backgroundColor = "#69809e";
+            document.getElementById("minus").style.backgroundColor = "#69809e";
+            document.getElementById("times").style.backgroundColor = "#69809e";
+            document.getElementById("divide").style.backgroundColor = "#95c5ac";
+            n = "÷"
+            visorDisplay = []
             break;
+
+    }
+})
 ```
